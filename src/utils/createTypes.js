98a9b4c12cc732ffdd822664252bdb3f3faa8663
Peter Schussheim <peter@schussheim.com>
2018-05-25 20:45:48 -0400

    comment out for now

--- src/utils/createTypes.js
+++ src/utils/createTypes.js
@@ -1,19 +1,19 @@
-/* @flow */
-const { generateNamespace } = require('@gql2ts/from-schema')
-const { DEFAULT_OPTIONS: flowDefaults } = require('gql2ts')
-const fs = require('fs')
-const { generateSchema } = require('./generateSchema')
-const myNamespace = generateNamespace(
-  'Gamma',
-  generateSchema(),
-  { ingnoreTypeNameDeclaration: true },
-  {
-    ...flowDefaults,
-    interfaceBuilder: (name, body) => `export type ${name} = ${body}`,
-    typeBuilder: (name, body) => `export type ${name} = ${body}`,
-    generateNamespace: (namespaceName, interfaces) => interfaces,
-    generateInterfaceName: name => `${name}`
-  }
-)
+// /* @flow */
+// const { generateNamespace } = require('@gql2ts/from-schema')
+// const { DEFAULT_OPTIONS: flowDefaults } = require('gql2ts')
+// const fs = require('fs')
+// const { generateSchema } = require('./generateSchema')
+// const myNamespace = generateNamespace(
+//   'Gamma',
+//   generateSchema(),
+//   { ingnoreTypeNameDeclaration: true },
+//   {
+//     ...flowDefaults,
+//     interfaceBuilder: (name, body) => `export type ${name} = ${body}`,
+//     typeBuilder: (name, body) => `export type ${name} = ${body}`,
+//     generateNamespace: (namespaceName, interfaces) => interfaces,
+//     generateInterfaceName: name => `${name}`
+//   }
+// )
 
-fs.writeFileSync('./src/generated/flowTypes.js', myNamespace)
+// fs.writeFileSync('./src/generated/flowTypes.js', myNamespace)
---
+++ src/utils/getUserId.js
@@ -0,0 +1,16 @@
+/* @flow */
+// import type { GraphQLContext } from '../flowTypes'
+
+// export function getUserId(ctx: GraphQLContext) {
+//   if (ctx.request.session.userId) {
+//     return ctx.request.session.userId
+//   }
+
+//   throw new AuthError()
+// }
+
+// export class AuthError extends Error {
+//   constructor() {
+//     super('Not authorized')
+//   }
+// }
