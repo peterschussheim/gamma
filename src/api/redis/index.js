86fbf7afb38695ee57466281b8260f8cb6028197
Peter Schussheim <peter@schussheim.com>
2018-05-25 20:44:56 -0400

    use `redis` client instead of `ioredis`

--- src/api/redis/index.js
+++ src/api/redis/index.js
@@ -1,11 +1,11 @@
 const logger = require('debug')('api:redis')
-import Redis from 'ioredis'
+import redis from 'redis'
 
 logger(`Connecting to Redis instance at: ${process.env.REDISLABS_URI}`)
 
 /**
  * Use in ApolloServer and pass into server's context object.
  */
-export const redis = new Redis(
+export const redisInstance = redis.createClient(
   `redis://:${process.env.REDISLABS_PW}@${process.env.REDISLABS_URI}`
 )
