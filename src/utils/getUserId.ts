f93b4da2156f6bbfc3cc0e9f930a5b74c091ab2c
Peter Schussheim <peter@schussheim.com>
2018-06-03 16:33:29 -0400

    extract reusable TS types/interfaces/etc into declaration file

--- src/utils/getUserId.ts
+++ src/utils/getUserId.ts
@@ -1,19 +1,22 @@
 import * as jwt from 'jsonwebtoken'
-import { Prisma } from '../generated/prisma'
+import { Context } from '../gamma'
 
-export interface Context {
-  db: Prisma
-  request: any
-}
-
-export function getUserId(context) {
-  const Authorization = context.request.get('Authorization')
+export function getUserIdFromToken(ctx: Context): string {
+  const Authorization = ctx.req.get('Authorization')
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
     const { userId } = jwt.verify(token, process.env.APP_SECRET!) as {
       userId: string
     }
     return userId
+  }
+
+  throw new AuthError()
+}
+
+export function getUserIdFromSession(ctx: Context): string {
+  if (ctx.session.userId) {
+    return ctx.session.userId
   }
 
   throw new AuthError()
