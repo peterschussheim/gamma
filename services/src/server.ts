656caf65228b76e2fd1efba2b94e41e95eabf4e4
Peter Schussheim <peter@schussheim.com>
2018-06-08 19:38:01 -0400

    add specific routes for graphql

--- services/src/server.ts
+++ services/src/server.ts
@@ -2,19 +2,15 @@ const debug = require('debug')('api')
 debug('Server starting...')
 debug('logging with debug enabled!')
 import * as compression from 'compression'
-import * as passport from 'passport'
 import { GraphQLServer, PubSub } from 'graphql-yoga'
-import { Strategy } from 'passport-github2'
 import { ApolloEngine } from 'apollo-engine'
 
 import { Prisma } from './generated/prisma'
 import { resolvers, fragmentReplacements } from './resolvers'
 import Raven from './shared/raven'
-import { redisInstance } from './api/redis'
-import middlewares from './api/routes/middlewares'
+import { redisInstance } from './redis'
+import middlewares from './routes/middlewares'
 import { securityMiddleware } from './shared/middlewares/security'
-import authRoutes from './api/routes/auth'
-import apiRoutes from './api/routes/api'
 
 const PORT = parseInt(process.env.PORT, 10) || 4000
 
@@ -27,43 +23,6 @@ async function startServer() {
     secret: process.env.PRISMA_SECRET,
     debug: true
   })
-  passport.use(
-    new Strategy(
-      {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: '/auth/github/callback',
-        scope: ['user']
-      },
-      async (accessToken, _, profile, cb) => {
-        debug(profile)
-        const { id, emails, name } = profile
-
-        const query = db.query.user({ where: { id } })
-
-        let email: string | null = null
-        if (emails) {
-          email = emails[0].value
-          await db.query.user({ where: { email } })
-        }
-        let user = await query
-
-        // user does not exist in db, need to register them
-        if (!user) {
-          // user = await db.mutation.createUser({
-          //   data: {
-          //     firstName: name.givenName,
-          //     lastName: name.familyName,
-          //     email,
-          //     password: 123
-          //   }
-          // })
-        }
-
-        return cb(null, { id: user.id })
-      }
-    )
-  )
 
   const graphQLServer = new GraphQLServer({
     typeDefs: './src/schema.graphql',
@@ -82,42 +41,13 @@ async function startServer() {
   securityMiddleware(graphQLServer.express)
   graphQLServer.express.use(compression())
   graphQLServer.express.use(middlewares)
-
-  // work on auth route next
-  graphQLServer.express.use('/auth', authRoutes)
-  // graphQLServer.express.get(
-  //   '/auth/github',
-  //   passport.authenticate('github', { scope: ['user:email'] }),
-  //   function(req, res) {
-  //     // The request will be redirected to GitHub for authentication, so this
-  //     // function will not be called.
-  //   }
-  // )
-
-  // // GET /auth/github/callback
-  // //   Use passport.authenticate() as route middleware to authenticate the
-  // //   request.  If authentication fails, the user will be redirected back to the
-  // //   login page.  Otherwise, the primary route function will be called,
-  // //   which, in this example, will redirect the user to the home page.
-  // graphQLServer.express.get(
-  //   '/auth/github/callback',
-  //   passport.authenticate('github', { failureRedirect: '/login' }),
-  //   (req, res) => res.redirect('/')
-  // )
-
-  // graphQLServer.express.get('/logout', function(req, res) {
-  //   req.logout()
-  //   res.redirect('/')
-  // })
-  graphQLServer.express.use('/api', apiRoutes)
-
   graphQLServer.express.use((err, req, res, next) => {
     if (err) {
       debug.error(err)
       res
         .status(500)
         .send(
-          'Oops, something went wrong! Our engineers have been alerted and will fix this asap.'
+          'Something went wrong! Our engineers have been alerted and will fix this asap.'
         )
       Raven.captureException(err)
     } else {
@@ -125,20 +55,41 @@ async function startServer() {
     }
   })
 
+  /**
+   * Options for graphql application server (graphQLServer)
+   */
+  const options = {
+    port: PORT,
+    cors: {
+      credentials: true,
+      origin:
+        process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
+          ? ['https://gamma.app', /gamma-(\w|-)+\.now\.sh/g, /gamma\.app/]
+          : [/localhost/]
+    },
+    endpoint: '/graphql',
+    subscriptions: '/subscriptions',
+    playground: '/playground'
+  }
+
   if (process.env.ENGINE_API_KEY) {
     const engine = new ApolloEngine({
       apiKey: process.env.ENGINE_API_KEY
     })
-    const httpServer = graphQLServer.createHttpServer({
+    const optionsWithTracing = {
       tracing: true,
-      cacheControl: true
-    })
-    engine.listen({ port: PORT, httpServer, graphqlPaths: ['/'] }, () =>
-      debug(`Server with Apollo Engine running on http://localhost:${PORT}`)
+      cacheControl: true,
+      ...options
+    }
+    const httpServer = graphQLServer.createHttpServer(optionsWithTracing)
+    engine.listen(
+      { port: PORT, httpServer, graphqlPaths: ['/graphql', '/subscriptions'] },
+      () =>
+        debug(`Server with Apollo Engine running on http://localhost:${PORT}`)
     )
   } else {
-    graphQLServer.start({ port: PORT }, () =>
-      debug(`Server running on http://localhost:${PORT}`)
+    graphQLServer.start(options, ({ port }) =>
+      debug(`Server running on http://localhost:${port}`)
     )
   }
 }
