18afe5b417e6d8283c77578522bec4c356f73e3b
Peter Schussheim <peter@schussheim.com>
2018-06-08 19:42:17 -0400

    make ordering consistent with express

--- services/src/shared/middlewares/cors.ts
+++ services/src/shared/middlewares/cors.ts
@@ -1,9 +1,9 @@
 import * as cors from 'cors'
 
 export default cors({
+  credentials: true,
   origin:
     process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
       ? ['https://gamma.app', /gamma-(\w|-)+\.now\.sh/g, /gamma\.app/]
-      : [/localhost/],
-  credentials: true
+      : [/localhost/]
 })
