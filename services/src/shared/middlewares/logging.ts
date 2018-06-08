17bfd3176390815d859fafa3983aee6613b663a2
Peter Schussheim <peter@schussheim.com>
2018-06-08 19:40:12 -0400

    remove unused debug calls

--- services/src/shared/middlewares/logging.ts
+++ services/src/shared/middlewares/logging.ts
@@ -2,7 +2,5 @@ const debug = require('debug')('shared:middlewares:logging')
 
 module.exports = (req, res, next) => {
   debug(`requesting ${req.ip}`)
-  debug(`req.user: ${req.user}`)
-  debug(`req.request: ${req.request}`)
   next()
 }
