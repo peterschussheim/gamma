11d42b7edb39aab69aebb66955291140be585378
Peter Schussheim <peter@schussheim.com>
2018-06-15 11:03:12 -0400

    services: fix user from token fn

--- services/src/utils/getUserId.ts
+++ services/src/utils/getUserId.ts
@@ -2,23 +2,24 @@ const debug = require('debug')('utils:getUserId')
 import * as jwt from 'jsonwebtoken'
 import { Context } from '../gamma'
 var util = require('util')
+
 /**
  * Gets user id from token
  * @param ctx
  * @returns string user id from token
  */
-// export function getUserIdFromToken(ctx: Context): string {
-//   const Authorization = ctx.req.get('Authorization')
-//   if (Authorization) {
-//     const token = Authorization.replace('Bearer ', '')
-//     const { userId } = jwt.verify(token, process.env.APP_SECRET!) as {
-//       userId: string
-//     }
-//     return userId
-//   }
+export function getUserIdFromToken(ctx: Context): string {
+  const Authorization = ctx.request.get('Authorization')
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, process.env.APP_SECRET!) as {
+      userId: string
+    }
+    return userId
+  }
 
-//   throw new AuthError(`Could not get userId from token`)
-// }
+  throw new AuthError(`Could not get userId from token`)
+}
 
 /**
  * Gets user id from session
@@ -35,9 +36,9 @@ export function getUserIdFromSession(ctx: Context): string {
    * 2) user sends login mutation and if successful, express server .....
    *
    */
-  debug(`ctx.req: ${util.inspect(ctx.req.session)}`)
-  if (ctx.req.session.userId) {
-    return ctx.req.session.userId
+  debug(`ctx.req: ${util.inspect(ctx.request.session)}`)
+  if (ctx.request.session.userId) {
+    return ctx.request.session.userId
   }
 
   throw new AuthError(`Could not get userId from session`)
