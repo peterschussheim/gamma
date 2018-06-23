0584c6a7c543b0b3077f25c21727d5dab45f4101
Peter Schussheim <peter@schussheim.com>
2018-06-23 17:29:49 -0400

    services: uppercase constants

--- services/src/constants.ts
+++ services/src/constants.ts
@@ -1,3 +1,3 @@
-export const redisSessionPrefix = 'sess:'
-export const userSessionIdPrefix = 'uid:'
-export const forgotPasswordPrefix = 'forgotpw:'
+export const REDIS_SESSION_PREFIX = 'sess:'
+export const USER_SESSION_ID_PREFIX = 'uid:'
+export const FORGOT_PW_PREFIX = 'forgotpw:'
