d514299bec6740a16f8e560d4ace5c1b915be359
Peter Schussheim <peter@schussheim.com>
2018-06-25 15:00:55 -0400

    services: refactor logic for removing one and all user sessions from redis

--- packages/services/src/utils/sessions.ts
+++ packages/services/src/utils/sessions.ts
@@ -1,9 +1,23 @@
 const debug = require('debug')('utils:sessions')
 
-import { USER_SESSION_ID_PREFIX, REDIS_SESSION_PREFIX } from '../constants'
+import {
+  USER_SESSION_ID_PREFIX,
+  REDIS_SESSION_PREFIX,
+  USER_SESSION_ID_PREFIX_TEST,
+  REDIS_SESSION_PREFIX_TEST
+} from '../constants'
 import { Redis } from 'ioredis'
 
 /**
+ * if process.env=test use ****_TEST prefix
+ * 
+ *  need to figure out a mechanism to test this better, auth mutations
+ *  should use redis test prefixes as well.
+ *
+/**
+
+
+ /** 
  * Deletes a SINGLE session for a given user.  Used to logout of a single session
  * (ex: user is signed in on 3 devices, logging out should only clear the session that the user
  * sent the logout mutation from.)
@@ -11,9 +25,16 @@ import { Redis } from 'ioredis'
  * @param sessionId {string}
  * @param redis {Redis}
  */
-export const removeSingleSession = async (sessionID: string, redis: Redis) => {
-  const sessionToRemove = await redis.del(`${REDIS_SESSION_PREFIX}${sessionID}`)
-  debug(`sessiontoremove: ${sessionToRemove}`)
+export const removeSingleSession = async (
+  sessionID: string,
+  userId: string,
+  redis: Redis
+) => {
+  // remove `sessionID` from sess keyspace
+  await redis.del(`${REDIS_SESSION_PREFIX}${sessionID}`)
+
+  // remove the same `sessionID` from this user's set of sessions
+  await redis.srem(`${USER_SESSION_ID_PREFIX}${userId}`, sessionID)
 }
 
 /**
@@ -25,15 +46,11 @@ export const removeSingleSession = async (sessionID: string, redis: Redis) => {
  * @param redis {Redis}
  */
 export const removeAllUserSessions = async (userId: string, redis: Redis) => {
-  // TODO: refactor to use a set operation
-  const sessionIds = await redis.lrange(
-    `${USER_SESSION_ID_PREFIX}${userId}`,
-    0,
-    -1
-  )
+  // get a list of all sessionIDs for this userID
+  const sessionIds = await redis.smembers(`${USER_SESSION_ID_PREFIX}${userId}`)
 
-  // delete each sessionID (`sess:9OSSlGnGCNFII7WhaI9tOwvvl1pf_gWn`)
-  // for this user
+  // delete each sessionID for this user
+  // `sess:9OSSlGnGCNFII7WhaI9tOwvvl1pf_gWn`
   await Promise.all(
     sessionIds.map(async id => {
       redis.del(`${REDIS_SESSION_PREFIX}${id}`)
@@ -41,27 +58,6 @@ export const removeAllUserSessions = async (userId: string, redis: Redis) => {
   )
 
   // delete the entire set of sessions for this user
-  // (`usid:cjinb8g7j9znj0886y207ieg4,NPISWP9f8qNuB7WHwjI8s09vT275qyvC`)
-  // find proper set method:
-  //
-  // await redis.srem(`usid:cjinb8g7j9znj0886y207ieg4')
+  // `usid:cjinb8g7j9znj0886y207ieg4`
+  await redis.del(`${USER_SESSION_ID_PREFIX}${userId}`)
 }
-
-// // refactor to use `p-queue`
-// export const clearUserSessions = async (userId: string, redis: Redis) => {
-//   // 1) find all sessions for this user
-//   const { sessionIds } = redis.lrange(
-//     `${USER_SESSION_ID_PREFIX}${userId}`,
-//     0,
-//     -1
-//   )
-
-//   // hold each `task`, to be Primise.all()
-//   const promises = []
-
-//   for (let i = 0; i < sessionIds; i += 1) {
-//     // push each 'deletion task' that meets into an array
-//     promises.push(redis.del(`${REDIS_SESSION_PREFIX}${sessionIds[i]}`))
-//   }
-//   await Promise.all(promises)
-// }
