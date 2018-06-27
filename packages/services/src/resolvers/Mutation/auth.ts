fd6029f0a876ca488238e44974ae350ecfa4b659
Peter Schussheim <peter@schussheim.com>
2018-06-26 20:29:48 -0400

    services: start adding pragrammatic emails to auth flows

--- packages/services/src/resolvers/Mutation/auth.ts
+++ packages/services/src/resolvers/Mutation/auth.ts
@@ -4,10 +4,13 @@ import * as jwt from 'jsonwebtoken'
 
 import { getUserIdFromSession, AuthError } from '../../utils/getUserId'
 import { Context } from '../../gamma'
+import { sendConfirmationEmail } from '../../utils/email'
+import { createConfirmEmailLink } from '../../utils/createConfirmEmailLink'
 import {
   removeSingleSession,
   removeAllUserSessions
 } from '../../utils/sessions'
+
 import {
   AccountNotFoundError,
   ConfirmationTokenExpiredError,
@@ -17,7 +20,7 @@ import {
   PostNotFoundError,
   PwResetTokenExpiredError
 } from '../../utils/errors'
-const assert = require('assert')
+
 import { USER_SESSION_ID_PREFIX } from '../../constants'
 
 export const auth = {
@@ -27,37 +30,54 @@ export const auth = {
       data: { ...args, password }
     })
 
+    // TODO: add better argument validation
     if (!user) {
       throw new EmailInUseError()
     }
 
-    ctx.request.session.userId = user.id
-
-    if (ctx.request.sessionID) {
-      debug(
-        `Pushing current request sessionID into redis set: ${
-          ctx.request.sessionID
-        }`
+    if (process.env.NODE_ENV !== 'test') {
+      const confirmationUrl = await createConfirmEmailLink(
+        ctx.url,
+        user.id,
+        ctx.redis
       )
-      // 4) create and/or add a new set for this user and add a the request.sessionID.
-      await ctx.redis.sadd(
-        `${USER_SESSION_ID_PREFIX}${user.id}`,
-        ctx.request.sessionID
-      )
-    }
-    debug('New user created')
-    debug('Returning signed token and user data')
 
-    return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
-      user
+      await sendConfirmationEmail(user.email, confirmationUrl)
+      debug(`Confirmation email sent to ${user.email}`)
+      return { success: true }
     }
+    return { success: false }
+
+    // ctx.request.session.userId = user.id
+    // if (ctx.request.sessionID) {
+    //   debug(
+    //     `Pushing current request sessionID into redis set: ${
+    //       ctx.request.sessionID
+    //     }`
+    //   )
+    //   // create and/or add a new set for this user and add the request.sessionID
+    //   await ctx.redis.sadd(
+    //     `${USER_SESSION_ID_PREFIX}${user.id}`,
+    //     ctx.request.sessionID
+    //   )
+    // }
+    // debug('New user created')
+    // debug('Returning signed token and user data')
+
+    // return {
+    //   token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+    //   user
+    // }
   },
 
   login: async (parent, args, ctx: Context, info) => {
     // 1) Lookup given email in db using `where` clause (email is unique field)
     const user = await ctx.db.query.user({ where: { email: args.email } })
     const valid = await bcrypt.compare(args.password, user ? user.password : '')
+
+    if (!user.emailConfirmed) {
+      throw new ConfirmEmailError()
+    }
 
     // 2) throw if user is not found or pw is invalid
     if (!valid || !user) {
