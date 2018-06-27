9dc1ebb2b9c91e48626249127a2c4d34ee69e787
Peter Schussheim <peter@schussheim.com>
2018-06-27 17:00:04 -0400

    services: continue refactor of auth flows and tests

---
+++ packages/services/src/routes/index.ts
@@ -0,0 +1,20 @@
+import * as express from 'express'
+// import setupUserRoutes from './users'
+import setupAuthRoutes from './auth'
+// import setupPostRoutes from './posts'
+
+function setupRoutes(app) {
+  const authRouter = express.Router()
+  setupAuthRoutes(authRouter)
+  app.use('/confirm/:id', authRouter)
+
+  // const userRouter = express.Router()
+  // setupUserRoutes(userRouter)
+  // app.use('/api/users', userRouter)
+
+  // const postRouter = express.Router()
+  // setupPostRoutes(postRouter)
+  // app.use('/api/posts', postRouter)
+}
+
+export default setupRoutes
