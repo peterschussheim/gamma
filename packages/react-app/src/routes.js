0ab8147d45b52068503304bd8723dc7707509cba
Peter Schussheim <peter@schussheim.com>
2018-07-29 16:57:21 -0400

    start building main Routes component

---
+++ packages/react-app/src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route, Switch, Redirect } from 'react-router'
+import Loadable from 'react-loadable'
+
+class Routes extends React.Component {
+  render() {
+    return <div />
+  }
+}
+
+export default Routes
