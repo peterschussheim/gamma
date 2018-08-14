169efd041c3377c9714e9b617edcc04ffb0d1d6b
Peter Schussheim <peter@schussheim.com>
2018-08-14 15:15:41 -0400

    shared: simplify package contents

--- packages/shared/src/raven.js
+++ packages/shared/src/raven.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: '../' })
 const debug = require('debug')('shared:raven')
-// import config from '../config'
 const { NODE_ENV, FORCE_DEV, SENTRY_DSN_SERVER } = process.env
 
 let Raven
