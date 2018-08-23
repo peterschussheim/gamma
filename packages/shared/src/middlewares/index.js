b838c58a2eb9623e84712ae2db35e00fa00f7754
Peter Schussheim <peter@schussheim.com>
2018-08-23 14:59:05 -0400

    shared: add middleswares/index top-level export

---
+++ packages/shared/src/middlewares/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cors: require('./cors'),
+  logging: require('./logging'),
+  raven: require('./raven'),
+  securityMiddleware: require('./securityMiddleware'),
+  session: require('./session'),
+  toobusy: require('./toobusy')
+}
