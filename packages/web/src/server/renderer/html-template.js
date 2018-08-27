65234389806165f3f474f42336b07d52fe9e9906
Peter Schussheim <peter@schussheim.com>
2018-08-27 16:41:00 -0400

    web: move ssr code into web source dir

--- packages/web/src/server/renderer/html-template.js
+++ packages/web/src/server/renderer/html-template.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/first */
 const debug = require('debug')('ssr:html-template')
 import fs from 'fs'
 import path from 'path'
@@ -6,26 +7,8 @@ import serialize from 'serialize-javascript'
 
 // Match main.asdf123.js in production mode or bundle.js in dev mode
 const mainBundleRegex = /(main|bundle)\.(?:.*\.)?js$/
-let bundles
-try {
-  bundles = fs.readdirSync(
-    path.resolve(__dirname, '..', '..', 'packages', 'web', 'build/static/js')
-  )
-} catch (err) {
-  debug(err)
-  throw new Error(
-    'You didn\'t run "yarn run dev:web" or "yarn run build:web" before starting the rendering service.\nPlease wait until either of them completes before starting the rendering service.'
-  )
-}
 
-// Get the main bundle filename
-const mainBundle = bundles.find(bundle => mainBundleRegex.test(bundle))
-if (!mainBundle) {
-  debug(err)
-  throw new Error(
-    'You didn\'t run "yarn run dev:web" or "yarn run build:web" before starting the rendering service.\nPlease wait until either of them completes before starting the rendering service.'
-  )
-}
+const assets = require(process.env.GAMMA_ASSETS_MANIFEST)
 
 export const createScriptTag = ({ src }) =>
   `<script defer="defer" src="${src}"></script>`
@@ -49,7 +32,7 @@ export const getHeader = ({ metaTags }) => {
           <div id="root">`
 }
 
-export const getFooter = ({ state, data, bundles }) => {
+export const getFooter = ({ state, data, bundles, chunks }) => {
   return html`</div>
       <script defer="defer" src="https://cdn.ravenjs.com/3.14.0/raven.min.js" crossorigin="anonymous"></script>
       <script defer="defer" src="/install-raven.js"></script>
@@ -57,8 +40,29 @@ export const getFooter = ({ state, data, bundles }) => {
       <script>window.__DATA__=${serialize(data)}</script>
       <script defer="defer" type="text/javascript" src="https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,Symbol.iterator"></script>
       <script type="text/javascript" src="/static/js/bootstrap.js"></script>
+      ${
+        process.env.NODE_ENV === 'production'
+          ? `<script src="${assets.client.js}"></script>`
+          : `<script src="${assets.client.js}" crossorigin></script>`
+      }
+      ${chunks
+        .map(
+          chunk =>
+            process.env.NODE_ENV === 'production'
+              ? `<script src="/${chunk.file}"></script>`
+              : `<script src="http://${process.env.HOST}:${parseInt(
+                  process.env.PORT,
+                  10
+                ) + 1}/${chunk.file}"></script>`
+        )
+        .join('\n')}  
       ${bundles.map(src => createScriptTag({ src }))}
-      ${createScriptTag({ src: `/static/js/${mainBundle}` })}
+      ${createScriptTag({
+        src: `/public/static/js/${bundles.find(bundle =>
+          mainBundleRegex.test(bundle)
+        )}`
+      })}
+      <script>window.main();</script>
     </body>
     </html>
   `
--- packages/web/src/server/renderer/index.js
+++ packages/web/src/server/renderer/index.js
@@ -1,4 +1,5 @@
-const debug = require('debug')('renderer:index')
+/* eslint-disable import/first */
+const debug = require('debug')('web:renderer:handler')
 import React from 'react'
 import { renderToNodeStream } from 'react-dom/server'
 import { ApolloProvider, getDataFromTree } from 'react-apollo'
@@ -9,21 +10,20 @@ import { StaticRouter } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
 import Loadable from 'react-loadable'
 import { getBundles } from 'react-loadable/webpack'
-
-// import Raven from 'shared'
-import stats from 'stats'
-
-import { getFooter, getHeader } from './html-template'
-import createCacheStream from '../create-cache-stream'
-
+import Raven from 'shared/src/raven'
+import 'cross-fetch/polyfill'
 // Browser shim has to come before any client imports
 import './browser-shim'
-const Routes = require('../../src/routes').default
+import { getFooter, getHeader } from './html-template'
+const assets = require(process.env.GAMMA_ASSETS_MANIFEST)
+// import createCacheStream from '../create-cache-stream'
 
-const IS_PROD = process.env.NODE_ENV === 'production'
+import Routes from '../../routes'
+const util = require('util')
 const FORCE_DEV = process.env.FORCE_DEV
+const IS_PROD = process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
 
-if (!IS_PROD || FORCE_DEV) debug('Querying API at localhost:4001/api')
+// if (!IS_PROD || FORCE_DEV) debug(`Querying API at ${API_URI}`)
 
 export const clearApolloStore = apolloClient => {
   try {
@@ -34,16 +34,19 @@ export const clearApolloStore = apolloClient => {
 }
 
 const renderer = (req, res) => {
+  const API_URI = IS_PROD
+    ? `https://${req.hostname}/api`
+    : 'http://localhost:4000/api'
+  const WS_URI = IS_PROD
+    ? `wss://${window.location.host}/subscriptions`
+    : 'ws://localhost:4000/subscriptions'
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-  debug(`server-side render ${req.url}`)
-  debug(`querying API at https://${req.hostname}/api`)
+  debug(`server-side render ${util.inspect(req.url)}`)
+  debug(`Rendering querying API at ${API_URI}`)
 
   const httpLink = createHttpLink({
-    uri:
-      IS_PROD && !FORCE_DEV
-        ? `https://${req.hostname}/api`
-        : 'http://localhost:4000/api',
+    uri: API_URI,
     credentials: 'include',
     headers: {
       cookie: req.headers.cookie
@@ -95,10 +98,11 @@ const renderer = (req, res) => {
       const { helmet } = helmetContext
       debug('write header')
       let response = res
-      if (!req.user) {
-        response = createCacheStream(req.path)
-        response.pipe(res)
-      }
+      // TODO: uncomment when cache is implemented
+      // if (!req.user) {
+      //   response = createCacheStream(req.path)
+      //   response.pipe(res)
+      // }
 
       response.write(
         getHeader({
@@ -115,18 +119,20 @@ const renderer = (req, res) => {
         response,
         { end: false }
       )
-
+      const stats = require('../../../build/react-loadable.json')
       const bundles = getBundles(stats, modules)
-        // Create <script defer> tags from bundle objects
-        .map(bundle => `/${bundle.file.replace(/\.map$/, '')}`)
-        // Make sure only unique bundles are included
-        .filter((value, index, self) => self.indexOf(value) === index)
+      const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'))
+      // // Create <script defer> tags from bundle objects
+      // .map(bundle => `/${bundle.file.replace(/\.map$/, '')}`)
+      // // Make sure only unique bundles are included
+      // .filter((value, index, self) => self.indexOf(value) === index)
       debug('bundles used:', bundles.join(','))
       stream.on('end', () =>
         response.end(
           getFooter({
             data,
-            bundles
+            bundles,
+            chunks
           })
         )
       )
