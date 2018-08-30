47320a68136c4d3d43ddb9c9ad6313fa8b854b48
Peter Schussheim <peter@schussheim.com>
2018-08-30 13:55:42 -0400

    web: use new shared apollo config

--- packages/web/src/client.js
+++ packages/web/src/client.js
@@ -3,53 +3,22 @@ import ReactDOM from 'react-dom'
 import Loadable from 'react-loadable'
 import { Router } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
+import { ApolloProvider } from 'react-apollo'
 
-import ApolloClient from 'apollo-client'
-import { ApolloProvider, getDataFromTree } from 'react-apollo'
-import { ApolloLink } from 'apollo-link'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import Routes from './hot-routes'
 import { history } from './utils/history'
-
-import {
-  errorLink,
-  queryOrMutationLink,
-  subscriptionLink,
-  requestLink
-} from './config/links'
-
-const NODE_ENV = process.env.NODE_ENV || 'development'
-const IS_PROD = process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
-const API_URI = IS_PROD ? '/api' : 'http://localhost:4000/api'
-const WS_URI = IS_PROD
-  ? `wss://${window.location.host}/subscriptions`
-  : 'ws://localhost:4000/subscriptions'
-
-const links = [
-  errorLink,
-  requestLink({
-    queryOrMutationLink: queryOrMutationLink({
-      uri: API_URI
-    }),
-    subscriptionLink: subscriptionLink({ uri: WS_URI })
-  })
-]
-
-const client = new ApolloClient({
-  ssrForceFetchDelay: 100,
-  link: ApolloLink.from(links),
-  cache: new InMemoryCache().restore(window.__APOLLO_STATE__)
-})
-
+import { API_URI, WS_URI, IS_PROD } from './constants'
+import { client } from './config/apollo'
+console.log(`[CLIENT] apollo ssr enabled: ${client.ssrMode}`)
 const App = () => {
   return (
-    <HelmetProvider>
-      <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
         <Router history={history}>
           <Routes />
         </Router>
-      </ApolloProvider>
-    </HelmetProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   )
 }
 
