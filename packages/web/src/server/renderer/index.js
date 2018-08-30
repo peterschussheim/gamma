544524a260bd7b7f9931d3d3074ca4efd7d9c3e3
Peter Schussheim <peter@schussheim.com>
2018-08-30 13:50:10 -0400

    web: simplify renderer middleware

--- packages/web/src/server/renderer/index.js
+++ packages/web/src/server/renderer/index.js
@@ -3,64 +3,30 @@ const debug = require('debug')('web:renderer:handler')
 import React from 'react'
 import { renderToNodeStream } from 'react-dom/server'
 import { ApolloProvider, getDataFromTree } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import { StaticRouter } from 'react-router'
 import { HelmetProvider } from 'react-helmet-async'
 import Loadable from 'react-loadable'
 import { getBundles } from 'react-loadable/webpack'
-import Raven from 'shared/src/raven'
 import 'cross-fetch/polyfill'
+import util from 'util'
+import Raven from 'shared/src/raven'
+
 // Browser shim has to come before any client imports
 import './browser-shim'
 import { getFooter, getHeader } from './html-template'
 const assets = require(process.env.GAMMA_ASSETS_MANIFEST)
 // import createCacheStream from '../create-cache-stream'
-
 import Routes from '../../routes'
-const util = require('util')
-const FORCE_DEV = process.env.FORCE_DEV
-const IS_PROD = process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
 
-// if (!IS_PROD || FORCE_DEV) debug(`Querying API at ${API_URI}`)
-
-export const clearApolloStore = apolloClient => {
-  try {
-    apolloClient.resetStore()
-  } catch (e) {
-    debug(`Error clearing apollo store: ${e}`)
-  }
-}
+import { API_URI } from '../../constants'
+import { client } from '../../config/apollo'
+console.log(`[SERVER] apollo ssr enabled: ${client.ssrMode}`)
 
 const renderer = (req, res) => {
-  const API_URI = IS_PROD
-    ? `https://${req.hostname}/api`
-    : 'http://localhost:4000/api'
-  const WS_URI = IS_PROD
-    ? `wss://${window.location.host}/subscriptions`
-    : 'ws://localhost:4000/subscriptions'
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-  debug(`server-side render ${util.inspect(req.url)}`)
-  debug(`Rendering querying API at ${API_URI}`)
-
-  const httpLink = createHttpLink({
-    uri: API_URI,
-    credentials: 'include',
-    headers: {
-      cookie: req.headers.cookie
-    }
-  })
-
-  const cache = new InMemoryCache({})
-
-  // Create an Apollo Client with a local network interface
-  const client = new ApolloClient({
-    ssrMode: true,
-    link: httpLink,
-    cache
-  })
+  debug(`server-side rendering path: ${util.inspect(req.url)}`)
+  debug(`Rendering service querying API at ${API_URI}`)
 
   let modules = []
   const report = moduleName => {
@@ -119,19 +85,17 @@ const renderer = (req, res) => {
         response,
         { end: false }
       )
+
       const stats = require('../../../build/react-loadable.json')
       const bundles = getBundles(stats, modules)
       const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'))
-      // // Create <script defer> tags from bundle objects
-      // .map(bundle => `/${bundle.file.replace(/\.map$/, '')}`)
-      // // Make sure only unique bundles are included
-      // .filter((value, index, self) => self.indexOf(value) === index)
-      debug('bundles used:', bundles.join(','))
+
+      debug('chunks used:', util.inspect(chunks))
+
       stream.on('end', () =>
         response.end(
           getFooter({
             data,
-            bundles,
             chunks
           })
         )
