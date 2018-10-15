00126c0c9a5b6ff12e0f77977270dbdf939163ba
Peter Schussheim <peter@schussheim.com>
2018-10-14 20:38:02 -0400

    fix typo

--- packages/web/src/config/isomorphicVariables.js
+++ packages/web/src/config/isomorphicVariables.js
@@ -2,7 +2,9 @@ export const runtimeConfig =
   typeof window !== 'undefined'
     ? {
         // use this on CLIENT-side (browser) apollo-client instances
-        CLIENT_WS_URI: process.env.API_STAGING_URL
+        // `window.env` will be populated in the browser by the `web`
+        // ssr service.
+        CLIENT_WS_URI: window.env.API_STAGING_URL
       }
     : {
         // use this on with apollo-client instances in a node environment.
