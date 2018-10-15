6e0d9a634af5480fd0928585f27bd348b982a9ef
Peter Schussheim <peter@schussheim.com>
2018-10-14 20:36:41 -0400

    web: add mechanism to inject api url for pr preview deploys

---
+++ packages/web/src/config/isomorphicVariables.js
@@ -0,0 +1,12 @@
+export const runtimeConfig =
+  typeof window !== 'undefined'
+    ? {
+        // use this on CLIENT-side (browser) apollo-client instances
+        CLIENT_WS_URI: process.env.API_STAGING_URL
+      }
+    : {
+        // use this on with apollo-client instances in a node environment.
+        // not yet in use, need to update how apollo-client instances
+        // are constructed.
+        NODE_WS_URI: process.env.API_STAGING_URL
+      }
