2b1131eaa3bf77181c3a25007ca90fc95210714d
Peter Schussheim <peter@schussheim.com>
2018-10-14 20:44:14 -0400

    web: fix variable naming mistake

--- packages/web/src/config/isomorphicVariables.js
+++ packages/web/src/config/isomorphicVariables.js
@@ -4,11 +4,11 @@ export const runtimeConfig =
         // use this on CLIENT-side (browser) apollo-client instances
         // `window.env` will be populated in the browser by the `web`
         // ssr service.
-        CLIENT_WS_URI: window.env.API_STAGING_URL
+        WS_URI: window.env.API_STAGING_URL
       }
     : {
         // use this on with apollo-client instances in a node environment.
         // not yet in use, need to update how apollo-client instances
         // are constructed.
-        NODE_WS_URI: process.env.API_STAGING_URL
+        WS_URI: process.env.API_STAGING_URL
       }
