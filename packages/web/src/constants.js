5903eea3fb471d17e44e4849b87077ef920375ae
Peter Schussheim <peter@schussheim.com>
2018-10-16 20:09:07 -0400

    web: remove unused constants

--- packages/web/src/constants.js
+++ packages/web/src/constants.js
@@ -6,15 +6,6 @@ export const FORGOT_PW_PREFIX = 'forgotpw:'
 export const HIT_COUNTER_PREFIX = 'htcount:'
 
 /**
- * redis prefixes to be used when running tests, same as production prefixes
- * except we prefix with a `t`.
- */
-export const REDIS_SESSION_PREFIX_TEST = 'tsess:'
-export const USER_SESSION_ID_PREFIX_TEST = 'tusid:'
-export const FORGOT_PW_PREFIX_TEST = 'tforgotpw:'
-export const HIT_COUNTER_PREFIX_TEST = 'thcount:'
-
-/**
  * The following exports are used to construct new apollo-client instances
  * on each request.
  */
