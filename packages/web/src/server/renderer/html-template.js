d4c350433421324c41e12a3d5e93d2760c052ca3
Peter Schussheim <peter@schussheim.com>
2018-10-21 16:38:13 -0400

    web: cleanup html-template

--- packages/web/src/server/renderer/html-template.js
+++ packages/web/src/server/renderer/html-template.js
@@ -2,95 +2,40 @@ import serialize from 'serialize-javascript'
 import { runtimeConfig } from '../../isomorphicVariables'
 const debug = require('debug')('ssr:html-template')
 const { inspect } = require('util')
-// import fs from 'fs'
-// import path from 'path'
-// import { html } from 'common-tags'
-debug(`html-template: ${inspect(runtimeConfig, { depth: 5, colors: true })}`)
-// debug(`process.env: ${inspect(process.env, { depth: 5, colors: true })}`)
-// // Match main.asdf123.js in production mode or bundle.js in dev mode
-// const mainBundleRegex = /(main|bundle)\.(?:.*\.)?js$/
+// debug(`html-template: ${inspect(runtimeConfig, { depth: 5, colors: true })}`)
 
-// export const createScriptTag = ({ src }) =>
-//   `<script type="text/javascript" defer="defer" src="${src}"></script>`
-
-// export const getHeader = ({ metaTags }) => {
-//   return html`
-//       <!DOCTYPE html>
-//       <html lang="en">
-//         <head>
-//           <meta charset="utf-8">
-//           <meta name="viewport" content="width=device-width,initial-scale=1">
-//           <meta name="og:type" content="website">
-//           <meta name="og:site_name" content="gamma.app">
-//           ${metaTags}
-//           <script>
-//               !function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject=g,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.defer=1,c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script",0,"ga"),ga("create","UA-123019519-1","auto"),ga("send","pageview"),ga('set', 'anonymizeIp', true)
-//           </script>
-//         </head>
-//         <body>
-//           <div id="root">`
-// }
-
-// export const getFooter = ({
-//   assets,
-//   state = {},
-//   data,
-//   scripts,
-//   staticBuild
-// }) => {
-//   return html`</div>
-//   ${
-//     process.env.NODE_ENV === 'production'
-//       ? `<script src="${assets.client.js}"></script>`
-//       : `<script src="${assets.client.js}" crossorigin></script>`
-//   }
-//   ${scripts
-//     .map(file => `<script src="${staticBuild}${file}"></script>`)
-//     .join('\n')}
-//       <script>window.__SERVER_STATE__=${serialize(state)}</script>
-//       <script>window.__DATA__=${serialize(data)}</script>
-//       <script defer="defer" type="text/javascript" src="https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,Symbol.iterator"></script>
-//       <script>window.main();</script>
-//     </body>
-//     </html>
-//   `
-// }
 export default ({ helmet, data, assets, scripts, staticBuild }) => {
-  const header = `
-  <!DOCTYPE html>
-  <html>
+  const header = `<!DOCTYPE html>
+    <html>
       <head>
-        <meta name="application-name" content="gamma UI">
-        <meta name="description" content="cool">
-        <meta char-set="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${helmet.title.toString()}
-        ${helmet.meta.toString()}
-        ${helmet.link.toString()}
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="og:type" content="website">
+        <meta name="og:site_name" content="gamma.app">
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+          ${helmet.link.toString()}
+        <script>
+            !function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject=g,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.defer=1,c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script",0,"ga"),ga("create","UA-123019519-1","auto"),ga("send","pageview"),ga('set', 'anonymizeIp', true)
+        </script>
       </head>
       <body>
-        <div id="root">`
+      <div id="root">`
+
   const footer = `</div>
         <script>window.env = ${serialize(runtimeConfig)};</script>
-        <script type="application/javascript">window.__DATA__=${JSON.stringify(
-          data
-        ).replace(/</g, '\\u003c')}</script>
+        <script >window.__DATA__=${serialize(data)}</script>
     ${
       process.env.NODE_ENV === 'production'
-        ? `<script type="application/javascript" src="${
-            assets.client.js
-          }"></script>`
+        ? `<script src="${assets.client.js}"></script>`
         : `<script src="${assets.client.js}" crossorigin></script>`
     }
     ${scripts
-      .map(
-        file =>
-          `<script type="application/javascript" src="${staticBuild}${file}"></script>`
-      )
+      .map(file => `<script src="${staticBuild}${file}"></script>`)
       .join('\n')}
       <script>window.main();</script>
       </body>
     </html>`
+
   return { header, footer }
 }
