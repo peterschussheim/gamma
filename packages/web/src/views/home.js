8094cb3efed3c48f90b00e9ce3a3dea6f3cbd21c
Peter Schussheim <peter@schussheim.com>
2018-10-21 15:55:14 -0400

    web: cleanup comments

--- packages/web/src/views/home.js
+++ packages/web/src/views/home.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Subscription } from 'react-apollo'
 import gql from 'graphql-tag'
 
-// import Post from '../components/post'
-
 const COUNTER_SUBSCRIPTION = gql`
   subscription {
     counter {
@@ -29,25 +27,5 @@ function Home() {
     </React.Fragment>
   )
 }
-
-// // function Timeline({ users = [], posts = [] }) {
-// //   return (
-// //     <div>
-// //       {posts
-// //         .sort(sortByLatest)
-// //         .map(p => (
-// //           <Post
-// //             key={p.id}
-// //             post={p}
-// //             author={users.find(u => u.id === p.authorId)}
-// //           />
-// //         ))}
-// //     </div>
-// //   )
-// // }
-
-// function sortByLatest(p1, p2) {
-//   return p1.date > p2.date ? -1 : 1
-// }
 
 export default Home
