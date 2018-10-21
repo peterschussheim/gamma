9fea72aba023087e36125d5ef0bdcf42cdfac5f4
Peter Schussheim <peter@schussheim.com>
2018-10-21 15:33:18 -0400

    add comments for later use

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -5,8 +5,11 @@ describe('Home page', () => {
     cy.visit('/')
   })
   it('Visits local gamma homepage', () => {
+    // TODO: fix below so they aren't using brittle selectors
     cy.get(':nth-child(3) > span')
     cy.get('p > a').should('be.visible')
     // websocket counter should be visible
+
+    // show 'web' build info
   })
 })
