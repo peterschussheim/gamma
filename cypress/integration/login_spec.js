813d7f255f85b1001bd04dec61dd80f97647913c
Peter Schussheim <peter@schussheim.com>
2018-10-28 16:53:35 -0400

    add more specs for authentication flows

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -3,9 +3,9 @@
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('/auth/login')
+    cy.get('[data-cy="authenticate-link"]').should('not.exist')
   })
   it('Authenticates an existing user', () => {
-    cy.get('[data-cy="authenticate-link"]').should('not.exist')
     cy.get('[data-cy="email-input"]')
       .type('peter@schussheim.com')
       .should('have.value', 'peter@schussheim.com')
@@ -17,5 +17,14 @@ describe('Login', () => {
     cy.get('[data-cy="counter-sub"]').should('be.visible')
     cy.get('[data-cy="logout-button"]').should('be.visible')
   })
-  xit('Validates form data on the client', () => {})
+  it('Renders error for non-existant email', () => {
+    cy.get('[data-cy="email-input"]')
+      .type('auntbee@mayberry.gov')
+      .should('have.value', 'auntbee@mayberry.gov')
+    cy.get('[data-cy="password-input"]')
+      .type('12345')
+      .should('have.value', '12345')
+    cy.get('[data-cy="login-button"]').click()
+    cy.get('[data-cy="graphql-errors"]').should('exist')
+  })
 })
--- cypress/integration/signup_spec.js
+++ cypress/integration/signup_spec.js
@@ -1,14 +1,38 @@
 /// <reference types="Cypress" />
 
-xdescribe('Signup', function() {
+describe('Signup', function() {
   beforeEach(() => {
     cy.visit('/auth/login')
+    cy.get('[data-cy="signup-button"]').click()
+    cy.get('[data-cy="authenticate-link"]').should('not.exist')
   })
-  it('Allows new users to signup', function() {
-    cy.get('a > button').click()
-    // enter email
-    // enter pw
-    // click register
+  xit('Allows new users to signup', function() {
+    cy.get('[data-cy="name-input"]')
+      .type('Peter')
+      .should('have.value', 'Peter')
+    cy.get('[data-cy="email-input"]')
+      .type('peter@schussheim.com')
+      .should('have.value', 'peter@schussheim.com')
+    cy.get('[data-cy="password-input"]')
+      .type('12345')
+      .should('have.value', '12345')
+    cy.get('[data-cy="signup-button"]').click()
+    // automatically update user 'emailConfirmed=true'
+  })
+  it('Enforces unique email constraint by rendering error', function() {
+    cy.get('[data-cy="name-input"]')
+      .type('Peter')
+      .should('have.value', 'Peter')
+    cy.get('[data-cy="email-input"]')
+      .type('peter@schussheim.com')
+      .should('have.value', 'peter@schussheim.com')
+    cy.get('[data-cy="password-input"]')
+      .type('12345')
+      .should('have.value', '12345')
+    cy.get('[data-cy="signup-button"]')
+      .click()
+      .wait(500)
+    cy.get('[data-cy="graphql-errors"]').should('exist')
     // automatically update user 'emailConfirmed=true'
   })
 })
