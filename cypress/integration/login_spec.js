e5182fd425bfdd3860d21cc2f17e643d3cc21ea7
Peter Schussheim <peter@schussheim.com>
2018-10-28 16:16:04 -0400

    update existing tests to use data selectors

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -2,20 +2,21 @@
 
 describe('Login', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/auth/login')
   })
   it('Authenticates an existing user', () => {
-    cy.get('p > a').click()
-    cy.get('#email-input')
+    // cy.wait(500)
+    cy.get('[data-cy="authenticate-link"]').should('not.exist')
+    cy.get('[data-cy="email-input"]')
       .type('peter@schussheim.com')
       .should('have.value', 'peter@schussheim.com')
-    cy.get('[placeholder="Password..."]')
+    cy.get('[data-cy="password-input"]')
       .type('12345')
       .should('have.value', '12345')
     cy.get('[data-cy="login-button"]').click()
-    cy.get('[data-cy="authenticate-link"]').should('be.visible')
+    cy.get('[data-cy="authenticate-link"]').should('not.exist')
     cy.get('[data-cy="counter-sub"]').should('be.visible')
     cy.get('[data-cy="logout-button"]').should('be.visible')
   })
-  it('Validates form data on the client', () => {})
+  xit('Validates form data on the client', () => {})
 })
