16b8f3aa7d2e46162ad33401541da52581f2b367
Peter Schussheim <peter@schussheim.com>
2018-11-09 11:55:51 -0500

    api: fix tsc error on post mutation

--- packages/api/src/resolvers/Mutation/post.ts
+++ packages/api/src/resolvers/Mutation/post.ts
@@ -1,9 +1,12 @@
 const debug = require('debug')('mutation:post')
 const util = require('util')
-import { forwardTo } from 'prisma-binding'
 import { getUserIdFromSession, AuthError } from '../../utils/getUserId'
 import { Context } from '../../gamma'
 
 export const post = {
-  createPost: forwardTo('db')
+  createPost: async (parent, args, ctx: Context, info) => {
+    const newPost = ctx.db.mutation.createPost({ data: args }, info)
+
+    return newPost
+  }
 }
