2f5b90fc225ac5a432e8a134aa03d5f3a7c39c6a
Peter Schussheim <peter@schussheim.com>
2018-11-14 12:08:34 -0500

    web: add react-select

--- packages/web/src/views/editor.js
+++ packages/web/src/views/editor.js
@@ -1,21 +1,20 @@
 import React from 'react'
-// import * as api from '../utils/api'
-import Form from '../components/form'
+import { compose, graphql } from 'react-apollo'
+import { Formik, Form } from 'formik'
+
+import CustomSelect from '../components/form/select'
 import { Input, TextArea } from '../components/form/inputs'
+import { Debug } from '../components/form/formDebugger'
 
 class Editor extends React.Component {
-  static defaultProps = {
-    // api
-  }
   handleSubmit = e => {
     e.preventDefault()
-    const { title, content, tags } = e.target.elements
+    const { title, content, tags, visibility } = e.target.elements
     const newPost = {
+      visibility: visibility.value,
       title: title.value,
       content: content.value,
-      tags: tags.value.split(',').map(t => t.trim()),
-      date: new Date().toISOString(),
-      authorId: this.props.user.id
+      tags: tags.value.split(',').map(t => t.trim())
     }
     this.props.api.posts.create(newPost).then(() => {
       this.props.history.push('/')
@@ -23,25 +22,68 @@ class Editor extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Form onSubmit={this.handleSubmit}>
-          <label style={{ justifySelf: 'right' }} htmlFor="title-input">
-            Title
-          </label>
-          <Input id="title-input" placeholder="Title" name="title" />
-          <label
-            style={{ justifySelf: 'right', alignSelf: 'baseline' }}
-            htmlFor="content-input"
-          >
-            Content
-          </label>
-          <TextArea id="content-input" placeholder="Content" name="content" />
-          <label style={{ justifySelf: 'right' }} htmlFor="tags-input">
-            Tags
-          </label>
-          <Input id="tags-input" placeholder="tags" name="tags" />
-        </Form>
-      </div>
+      <Formik
+        initialValues={{
+          visibility: 'PUBLIC',
+          content: {
+            name: '',
+            data: ''
+          }
+        }}
+        onSubmit={this.handleSubmit}
+        render={({
+          values,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleSubmit,
+          handleReset,
+          setFieldValue,
+          setFieldTouched
+        }) => (
+          <Form>
+            <label style={{ justifySelf: 'right' }} htmlFor="title-input">
+              Title
+            </label>
+            <Input id="title-input" placeholder="Title" name="title" />
+            <label
+              style={{ justifySelf: 'right', alignSelf: 'baseline' }}
+              htmlFor="content-input"
+            >
+              Content
+            </label>
+            <TextArea id="content-input" placeholder="Content" name="content" />
+            <label style={{ justifySelf: 'right' }} htmlFor="tags-input">
+              Tags
+            </label>
+            <Input id="tags-input" placeholder="tags" name="tags" />
+            <CustomSelect
+              value={values.visibility}
+              onChange={setFieldValue}
+              error={errors.visibility}
+              onBlur={setFieldTouched}
+            />
+            <button
+              type="submit"
+              className="secondary"
+              data-cy="submit-post"
+              disabled={!dirty || isSubmitting}
+            >
+              Update
+            </button>
+            <button
+              type="reset"
+              className="secondary"
+              disabled={isSubmitting}
+              onClick={handleReset}
+            >
+              Reset
+            </button>
+            <Debug />
+          </Form>
+        )}
+      />
     )
   }
 }
