4f727253af7ba10efb7dad3d78bdad6614542517
Peter Schussheim <peter@schussheim.com>
2018-11-16 16:08:52 -0500

    api: implement resolver for getGistById

--- packages/api/src/resolvers/Query.ts
+++ packages/api/src/resolvers/Query.ts
@@ -1,9 +1,43 @@
-import { Context } from '../gamma'
+const debug = require('debug')('resolvers:Query')
+const util = require('util')
+import * as Octokit from '@octokit/rest'
+import { UnauthenticatedError } from '../utils/errors'
 import { getUserIdFromSession } from '../utils/getUserId'
+import { Context } from '../gamma'
 
-// These queries can probably be refactored so that the authentication
-// logic is handled as a graphql 'middleware'.
 export const Query = {
+  getGistById: async (parent, args, ctx: Context, info) => {
+    const fragment = ` {
+      id
+      githubProfile {
+        accessTokens {
+          token
+        }
+      }
+    }
+  `
+    const id = getUserIdFromSession(ctx)
+    if (id === 'unauthenticated' || null) {
+      throw new UnauthenticatedError()
+    }
+    const {
+      githubProfile: { accessTokens }
+    } = await ctx.db.query.user({ where: { id } }, fragment)
+    const [token] = accessTokens
+    const { data } = await getGistById(token, { gist_id: args.gistId })
+    // debug(`gist response:  ${util.inspect(data)}`)
+    // tslint:disable-next-line:no-shadowed-variable
+    const { url, public: isPublic, id: gistId, description, truncated } = data
+
+    return {
+      url,
+      isPublic,
+      gistId,
+      files: [...getGistFiles(data)],
+      description,
+      truncated
+    }
+  },
   viewer: () => ({}),
   // TODO: add logic that allows a user to only view PUBLIC or their OWN posts
   postById: async (parent, args, ctx: Context, info) => {
@@ -13,7 +47,6 @@ export const Query = {
     const post = await ctx.db.query.post({ where: { id: args.id } }, info)
     return post
   },
-
   // Refactor so that `orderBy` gets its value from `args` instead of hardcoding
   postsByDate: async (parent, args, ctx: Context, info) => {
     if (!getUserIdFromSession(ctx)) {
@@ -21,7 +54,6 @@ export const Query = {
     }
     return ctx.db.query.posts({ orderBy: 'updatedAt_DESC' }, info)
   },
-
   tags: async (parent, args, ctx: Context, info) => {
     if (!getUserIdFromSession(ctx)) {
       ctx.res.send(401)
@@ -29,4 +61,29 @@ export const Query = {
     const tags = await ctx.db.query.tags({})
     return tags
   }
+}
+
+async function getGistById(
+  accessToken,
+  id: Octokit.GistsGetParams
+): Promise<any> {
+  const { token } = accessToken
+  const ghClient = new Octokit()
+  ghClient.authenticate({
+    type: 'oauth',
+    token
+  })
+  const result = await ghClient.gists.get(id)
+  return result
+}
+
+function getGistFiles(gist) {
+  const files = gist.files
+  const gistFiles = []
+  for (const [key, value] of Object.entries(files)) {
+    if (files.hasOwnProperty(key)) {
+      gistFiles.push(files[key])
+    }
+  }
+  return gistFiles
 }
