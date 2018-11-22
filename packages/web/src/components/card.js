1f7ab295a1f63609139ddde5b16632b4df9d1cac
Peter Schussheim <peter@schussheim.com>
2018-11-22 14:08:52 -0500

    web: add card component

---
+++ packages/web/src/components/card.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default class Card extends React.Component {
+  state = {
+    showMenu: false
+  }
+
+  showMenu = event => {
+    event.preventDefault()
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu)
+    })
+  }
+
+  closeMenu = event => {
+    if (!this.dropdownMenu.contains(event.target)) {
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu)
+      })
+    }
+  }
+
+  render() {
+    const { children } = this.props
+    return (
+      <React.Fragment>
+        {!this.state.showMenu ? (
+          <button onClick={this.showMenu}>Menu</button>
+        ) : (
+          <span style={{ fontSize: '30px' }}> ✕ </span>
+        )}
+
+        {this.state.showMenu ? (
+          <div
+            className="menu"
+            ref={element => {
+              this.dropdownMenu = element
+            }}
+          >
+            {children}
+          </div>
+        ) : null}
+      </React.Fragment>
+    )
+  }
+}
