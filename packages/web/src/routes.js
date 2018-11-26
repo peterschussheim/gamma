90aca26ed6529387d75b3091bfc2ed00ccd8022a
Peter Schussheim <peter@schussheim.com>
2018-11-26 11:11:20 -0500

    web: move global styles into sep file

--- packages/web/src/routes.js
+++ packages/web/src/routes.js
@@ -5,6 +5,7 @@ import { Global, css } from '@emotion/core'
 import { Query } from 'react-apollo'
 
 import { GET_GIST_BY_ID } from './queries'
+import globalStyles from './components/global'
 import Navbar from './components/navbar'
 import Login from './components/form/login'
 import Signup from './components/form/signup'
@@ -26,112 +27,7 @@ class Routes extends React.Component {
     // const { currentUser } = this.props
     return (
       <React.Fragment>
-        <Global
-          styles={css`
-            * {
-              box-sizing: border-box;
-              font-family: ;
-            }
-            :root {
-              --green: #5edb93;
-              --black: #414141;
-              --shadow: 0px 8px 30px -5px rgba(79, 79, 79, 0.3);
-              --shadowHover: 0px 8px 5px -5px rgba(79, 79, 79, 0.3);
-            }
-            body {
-              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
-                Helvetica, Arial, sans-serif, 'Apple Color Emoji',
-                'Segoe UI Emoji', 'Segoe UI Symbol';
-              font-size: 14px;
-              line-height: 1.5;
-              color: #24292e;
-              background-color: #fff;
-            }
-
-            a {
-              color: #08c;
-            }
-
-            code {
-              background: #eee;
-              padding: 0.1rem;
-              font-family: 'Menlo';
-              font-size: 13px;
-              color: #ff00aa;
-            }
-
-            #flex-container {
-              display: flex;
-              flex-direction: row;
-            }
-
-            #flex-container > .flex-item {
-              flex: auto;
-            }
-
-            #flex-container > .raw-item {
-              width: 5rem;
-            }
-
-            .text-input {
-              padding: 0.5rem;
-              font-size: 16px;
-              width: 100%;
-              display: block;
-              border-radius: 0px;
-              border: 1px solid #ccc;
-            }
-
-            .text-input:focus {
-              border-color: #007eff;
-              box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
-                0 0 0 3px rgba(0, 126, 255, 0.1);
-              outline: none;
-            }
-
-            .error .text-input {
-              border-color: red;
-            }
-
-            .label {
-              font-weight: bold;
-              display: block;
-              margin-bottom: 0.5rem;
-            }
-
-            .error .label {
-              color: red;
-            }
-
-            .input-feedback {
-              color: #999;
-              margin-top: 0.25rem;
-            }
-
-            .error .input-feedback {
-              color: red;
-            }
-
-            .input-group {
-              margin-bottom: 1rem;
-            }
-
-            button:disabled {
-              opacity: 0.5;
-              cursor: not-allowed !important;
-            }
-
-            button + button {
-              margin-left: 0.5rem;
-            }
-
-            button.outline {
-              background-color: #eee;
-              border: 1px solid #aaa;
-              color: #555;
-            }
-          `}
-        />
+        <Global styles={globalStyles} />
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
