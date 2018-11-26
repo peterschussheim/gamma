96077479d9e52d20809b541ddaa43e40613ee5f7
Peter Schussheim <peter@schussheim.com>
2018-11-26 11:15:05 -0500

    web: move icons to assets directory

--- packages/web/src/components/icon.js
+++ packages/web/src/components/icon.js
@@ -4,6 +4,6 @@ import { getIconForFile } from 'vscode-icons-js'
 
 export default function Icon({ name }) {
   const iconName = getIconForFile(name)
-  const iconPath = require(`../../static/icons/${iconName}`)
+  const iconPath = require(`../assets/icons/${iconName}`)
   return <img style={{ height: 20 }} src={iconPath} alt="file-type" />
 }
