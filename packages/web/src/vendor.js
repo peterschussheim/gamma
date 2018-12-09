1bf6552fae6079e7ca5428f53a01c5b994d9e0af
Peter Schussheim <peter@schussheim.com>
2018-12-09 11:32:17 -0500

    web: remove unused files

--- packages/web/src/vendor.js
+++
@@ -1,7 +0,0 @@
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
-
-const editor = monaco.editor
-const Range = monaco.Range
-const Severity = monaco.MarkerSeverity
-
-export { editor, Range, Severity }
