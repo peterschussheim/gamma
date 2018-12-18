46bd13586ebcf758ee85368e8a2ad8f1f45fc53e
Peter Schussheim <peter@schussheim.com>
2018-12-18 11:45:46 -0500

    web: return null if no file is loaded(icon)

--- packages/web/src/components/icon.tsx
+++ packages/web/src/components/icon.tsx
@@ -3,7 +3,12 @@ import * as React from 'react'
 import { getIconForFile } from 'vscode-icons-js'
 
 export default function Icon({ filename = 'index.js', height }) {
-  const iconName = getIconForFile(filename)
+  let iconName
+  try {
+    iconName = getIconForFile(filename)
+  } catch (error) {
+    return null
+  }
   const iconPath = require(`../../public/icons/${iconName}`)
   return <img style={{ height }} src={iconPath} alt="file-type" />
 }
