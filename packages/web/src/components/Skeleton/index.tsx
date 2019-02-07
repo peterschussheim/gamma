31e50141d6d8e39d667d0795c6ab6b7c02945fee
Peter Schussheim <peter@schussheim.com>
2019-02-07 13:43:32 -0500

    web: Skeleton accepts 'footer' prop

--- packages/web/src/components/Skeleton/index.tsx
+++ packages/web/src/components/Skeleton/index.tsx
@@ -1,25 +1,42 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
 import * as React from 'react'
-
-import { Container, LeftPanel, RightPanel } from './elements'
+import SplitPane from 'react-split-pane'
 
 type Props = {
   sidebar: React.ReactNode
   editor: React.ReactNode
+  footer: React.ReactNode
 }
 
 export default class Skeleton extends React.Component<Props> {
-  // shouldComponentUpdate() {
-  //   return false
-  // }
-
   render() {
-    const { sidebar, editor } = this.props
+    const { sidebar, editor, footer } = this.props
 
     return (
-      <Container>
-        <LeftPanel>{sidebar}</LeftPanel>
-        <RightPanel>{editor}</RightPanel>
-      </Container>
+      <React.Fragment>
+        <div css={container}>
+          <div css={leftPanel}>{sidebar}</div>
+          <div css={rightPanel}>{editor}</div>
+        </div>
+        <div>{footer}</div>
+      </React.Fragment>
     )
   }
 }
+
+const container = css`
+  display: flex;
+  min-height: 500px;
+  min-width: 800px;
+`
+const leftPanel = css`
+  flex: 1;
+  overflow: auto;
+  background-color: #001023;
+`
+const rightPanel = css`
+  display: flex;
+  flex-direction: column;
+  flex: 3;
+`
