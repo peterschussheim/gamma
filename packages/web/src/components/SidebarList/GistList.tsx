4eb6dca78f41a1add36bbb9e8bfd3d56c318f2d4
Peter Schussheim <peter@schussheim.com>
2019-02-10 15:55:26 -0500

    web: lift viewer gists query

--- packages/web/src/components/SidebarList/GistList.tsx
+++ packages/web/src/components/SidebarList/GistList.tsx
@@ -1,47 +1,38 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { graphql } from 'react-apollo'
 
 import { ViewerGists } from '../../__generated__/types'
-import { VIEWER_GISTS } from '../../queries'
 import { SidebarContainer, SidebarItem } from '../Sidebar/elements'
 
 interface Props {
   data: ViewerGists
 }
 
-const withGistList = graphql(VIEWER_GISTS, {
-  options: () => ({
-    fetchPolicy: 'cache-and-network'
-  })
-})
-
-class GistList extends React.PureComponent<Props> {
-  render() {
-    const { data } = this.props
-    // @ts-ignore
-    const { loading, error } = data
-    if (loading) {
-      return null
-    } else if (error) {
-      return <div>{error}</div>
-    } else {
-      return (
-        <SidebarContainer>
-          {data.viewer
-            ? data.viewer.gists.map(gist => (
-                <SidebarItem key={gist.gistId}>
-                  <Link to={`/g/${gist.gistId}`}>
-                    {gist.description || '[no description]'}
-                  </Link>
-                </SidebarItem>
-              ))
-            : null}
-        </SidebarContainer>
-      )
-    }
+const GistList: React.FC<Props> = ({ data }) => {
+  // @ts-ignore
+  if (data.networkStatus === 1) {
+    return <div>loading</div>
   }
+
+  // @ts-ignore
+  if (data.error) {
+    // @ts-ignore
+    return <div>{data.error.message}</div>
+  }
+  return (
+    <SidebarContainer>
+      {data.viewer
+        ? data.viewer.gists.map(gist => (
+            <SidebarItem key={gist.gistId}>
+              <Link to={`/g/${gist.gistId}`}>
+                {gist.description || '[no description]'}
+              </Link>
+            </SidebarItem>
+          ))
+        : null}
+    </SidebarContainer>
+  )
 }
 
 // @ts-ignore
-export default withGistList(GistList)
+export default GistList
