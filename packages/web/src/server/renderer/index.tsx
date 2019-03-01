1a3e2bcdced774bc34ee7873f07ec784287e57cd
Peter Schussheim <peter@schussheim.com>
2019-03-01 12:27:36 -0500

    web: remove loading of editor.main

--- packages/web/src/server/renderer/index.tsx
+++ packages/web/src/server/renderer/index.tsx
@@ -102,7 +102,6 @@ const renderer = (req, res) => {
                   return '/worker-loader-proxy.js';
                 }
               };
-              require(["vs/editor/editor.main"], function () {})
             </script>
             <script>
                 !function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject=g,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.defer=1,c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script",0,"ga"),ga("create","UA-123019519-1","auto"),ga("send","pageview"),ga('set', 'anonymizeIp', true)
