a79249de0e6a6bbba18d9bea45ab89be81107e18
Peter Schussheim <peter@schussheim.com>
2019-05-21 20:24:56 -0400

    web: remove counter component

--- packages/web/src/views/Home.tsx
+++ packages/web/src/views/Home.tsx
@@ -1,15 +1,31 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { css } from 'emotion'
 import * as React from 'react'
-
-import Counter from '../components/counter'
 
 function Home() {
   return (
-    <React.Fragment>
-      <div>HOME</div>
-      <span>
-        Count: <Counter />
-      </span>
-    </React.Fragment>
+    <div
+      className={css({
+        backgroundColor: '#0E2840',
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%'
+      })}
+    >
+      <h4
+        className={css({
+          color: '#89A5DB',
+          fontSize: '1.5em',
+          fontWeight: 500,
+          opacity: 1.0
+        })}
+      >
+        Select a file to view and edit
+      </h4>
+    </div>
   )
 }
 
